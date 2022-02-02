394b79c074334c889fbdeb043c5b448db61e3593
TeuTeuf <antoine.teuf@gmail.com>
2022-02-02 13:56:36 +0100

    add language in google map link to avoid promoting a random eletrician...

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -159,7 +159,10 @@ export function Game({ settingsData }: GameProps) {
             />
             <a
               className="underline w-full text-center block mt-4"
-              href={`https://www.google.com/maps?q=${country.name}`}
+              href={`https://www.google.com/maps?q=${getCountryName(
+                i18n.resolvedLanguage,
+                country
+              )}&hl=${i18n.resolvedLanguage}`}
               target="_blank"
               rel="noopener noreferrer"
             >
