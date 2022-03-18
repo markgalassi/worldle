37fd8f9c7cf44f7c05a8fac29384f507771145a7
teuteuf <antoine.teuf@gmail.com>
2022-03-18 23:37:50 +0100

    try to improve cache refresh

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -63,6 +63,8 @@ function registerValidSW(swUrl: string, config?: Config) {
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
+      registration.update();
+
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
