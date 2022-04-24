6a6e784880c2bd5af84a3c1c49112a9547d7c311
teuteuf <antoine.teuf@gmail.com>
2022-04-24 12:28:13 +0200

    unregister service worker

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,6 @@ import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 import ReactPWAInstallProvider from "@teuteuf/react-pwa-install";
-import { onServiceWorkerUpdate } from "@3m1/service-worker-updater";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,9 +19,8 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register({
-  onUpdate: onServiceWorkerUpdate,
-});
+// serviceWorkerRegistration.register({ onUpdate: onServiceWorkerUpdate });
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
