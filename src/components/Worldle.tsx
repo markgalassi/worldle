5adc4eb1c5d6a991f8b51235ee69c4a291ab00c0
Eldo Roshi <eldoroshi@users.noreply.github.com>
2022-05-22 18:11:20 +0200

    Update Worldle.tsx

--- src/components/Worldle.tsx
+++ src/components/Worldle.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Worldle() {
   return (
     <span className="font-bold">
-      WOR<span className="text-green-600">L</span>DLE
+      WORDLE<span className="text-green-600">GEO</span>GRAPHY.COM
     </span>
   );
 }
