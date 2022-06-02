2e90240530b38d98db250f7c29024e2aa84446b7
teuteuf <antoine.teuf@gmail.com>
2022-06-02 19:13:12 +0200

    add boolean to toggle twitch link

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -20,6 +20,7 @@ import { Twemoji } from "@teuteuf/react-emoji-render";
 import { countries } from "../domain/countries.position";
 import { useNewsNotifications } from "../hooks/useNewsNotifications";
 
+const ENABLE_TWITCH_LINK = true;
 const MAX_TRY_COUNT = 6;
 
 interface GameProps {
@@ -232,19 +233,21 @@ export function Game({ settingsData, updateSettings }: GameProps) {
                 />
               </a>
             </div>
-            <div className="flex flex-wrap gap-4 justify-center">
-              <a
-                className="underline text-center block mt-4 whitespace-nowrap"
-                href="https://www.twitch.tv/t3uteuf"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Twemoji
-                  text="More? Play on Twitch! 👾"
-                  options={{ className: "inline-block" }}
-                />
-              </a>
-            </div>
+            {ENABLE_TWITCH_LINK && (
+              <div className="flex flex-wrap gap-4 justify-center">
+                <a
+                  className="underline text-center block mt-4 whitespace-nowrap"
+                  href="https://www.twitch.tv/t3uteuf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Twemoji
+                    text="More? Play on Twitch! 👾"
+                    options={{ className: "inline-block" }}
+                  />
+                </a>
+              </div>
+            )}
           </>
         ) : (
           <form onSubmit={handleSubmit}>
